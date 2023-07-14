06bafa8d50011b11f22f5df34afe4a36efe7ecc0
Bilal Malik <bilalmalik42011@gmail.com>
2023-07-14 17:41:31 +0500

    Nav title updated

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
                     width={30}
                     height={30}
                     className="object-contain" />
-                <p className="logo_text">Promptopia</p>
+                <p className="logo_text">IdeaSparks</p>
             </Link>
 
             <div className="sm:flex hidden">
